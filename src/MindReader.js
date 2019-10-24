984e3dcc6b9a551ed5b171a2fd840ea77a3899d8
Robbie Gay <robbiecgay@gmail.com>
2019-10-24 16:43:55 -0400

    Almost done with Mind Reader, may need to use state to get game to reset

--- src/MindReader.js
+++ src/MindReader.js
@@ -5,25 +5,19 @@ import Button from './Button';
 class MindReader extends React.Component {
     constructor(props) {
         super(props);
-        this.handleClick = this.handleClick.bind(this);
+        this.nextPage = this.nextPage.bind(this);
         this.resetPage = this.resetPage.bind(this);
         this.state = {
             page: this.pageContent[0],
         }
     }
 
-    finalAns = 'TEST';
+    finalAns = 'DEFAULT';
     currentPage = 0;
 
-    /* 
-    Generate random numbers 0-99
-    Have the generated numbers be random EXCEPT for the 9 multiples
-    Find a way to break lines
-    */
-
-
-    genSym(num) {
-        let output = [];
+    genSym() {
+        let symArray = [];
+        let output = '';
         // Symbol options
         let symChar = ['=', '@', '*', '$', '%', '^', '&', '+', '#'];
         // Generates the idx of the answer
@@ -39,13 +33,17 @@ class MindReader extends React.Component {
             // If the sym is the same as the previous one, add a new symbol
             // Did this b/c if you simply generate a new random number you run the risk of them happening to be the same
             // And if you loop until you get a new number, you could (maybe - super unlikely) loop forever
-            if (sym === output[i - 1]) {sym = '!'};
-            output.push(sym);
+            if (sym === symArray[i - 1]) {sym = '!'};
+            symArray.push(sym);
         }
         // Replace every multiple of 9 with the answer
         for (let i = 0; i < 12; i++) {
-            output.splice((i * 9), 1, this.finalAns);
+            symArray.splice((i * 9), 1, this.finalAns);
         }
+        // Format the output into num --> sym
+        symArray.map((item, idx) => output += `${idx} --> ${item} `);
+        console.log(this.finalAns);
+        return output;
     }
 
     resetImg() {
@@ -53,14 +51,15 @@ class MindReader extends React.Component {
         return 'RESET';
     }
 
-    handleClick() {
+    nextPage() {
         this.currentPage++;
-        this.setState({ page: this.pageContent[this.currentPage] })
+        this.setState({ page: this.pageContent[this.currentPage] });
     }
 
     resetPage() {
+        this.genSym();
         this.currentPage = 0;
-        this.setState({ page: this.pageContent[this.currentPage] })
+        this.setState({ page: this.pageContent[this.currentPage] });
     }
 
     pageContent = [
@@ -68,7 +67,7 @@ class MindReader extends React.Component {
         { 'text': 'Pick a number from 01-99', 'subtext': 'When you have your number, click next', 'btn': 'NEXT', 'reset': `${this.resetImg()}` },
         { 'text': 'Add both digits together to get a new number', 'subtext': `Ex: 25 is 2 + 5 = 7 || Ex: 05 is 0 + 5 = 5 || Click next to proceed`, 'btn': 'NEXT', 'reset': `${this.resetImg()}` },
         { 'text': 'Subtract your new number from your old number', 'subtext': 'Ex: 25 - 07 = 18 || Ex: 05 - 05 = 0 || Click next to proceed', 'btn': 'NEXT', 'reset': `${this.resetImg()}` },
-        { 'text': `${this.genSym(100)}`, 'subtext': '', 'btn': 'REVEAL', 'reset': `${this.resetImg()}` },
+        { 'text': `${this.genSym()}`, 'subtext': '', 'btn': 'REVEAL', 'reset': `${this.resetImg()}` },
         { 'text': `${this.finalAns}`, 'subtext': `Your symbol is: ${this.finalAns}`, 'btn': '', 'reset': `${this.resetImg()}` },
     ]
 
@@ -76,11 +75,11 @@ class MindReader extends React.Component {
         return (
             <React.Fragment>
                 <TextBox text={this.state.page.text} />
-                <Button text={this.state.page.btn} onClick={this.handleClick} />
+                <Button text={this.state.page.btn} onClick={this.nextPage} />
                 <TextBox text={this.state.page.subtext} />
                 <Button text={this.state.page.reset} onClick={this.resetPage} />
             </React.Fragment>
-        )
+        );
     }
 }
 
@@ -89,6 +88,6 @@ export default MindReader;
 /* 
 Issues:
 - Change || to <br>
-
+- Won't update to a new number, might need to use state
 
 */
