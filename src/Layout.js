cbe698901483033bfe056f311136ef62fdd76f95
Robbie Gay <robbiecgay@gmail.com>
2020-01-01 21:09:53 -0500

    working on getting phone background img

--- src/Layout.js
+++ src/Layout.js
@@ -2,35 +2,37 @@ import React from 'react';
 import GetTime from './GetTime';
 import TicTacToe from './TicTacToe';
 import MindReader from './MindReader';
+import background from './background-1.jpg';
 
 // Adds a Bootstrap layout to the App
 
-class Layout extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className='container-fluid'>
-                    <div className='row'>
-                        <div className='col-0 col-sm-0 col-md-1 col-lg-2' />
-                        <div className='col-12 col-sm-12 col-md-10 col-lg-8 text-center'>
-                            <div className='p-2'>
-                                {/* <h1 className='text-success'>Digital Clock</h1>
-                                <GetTime type={'digiClock'} />
-                                <h1 className='text-info'>Countdown Timer</h1>
-                                <GetTime type={'countdown'} />
-                                <h1 className='text-warning'>Tic-Tac-Toe</h1>
-                                <TicTacToe /> */}
-                                <h1 className='text-primary'>Mind Reader</h1>
-                                <MindReader />
-                            </div>
+function Layout() {
+    return (
+        <>
+        {/* <div id="bckImg" className="container-fluid mt-3" style="background-image: url(img/chidi-the-good-place.jpg); background-repeat: no-repeat; min-height: 700px"></div> */}
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-0 col-sm-0 col-md-1 col-lg-2' />
+                    <div className='col-12 col-sm-12 col-md-10 col-lg-8 text-center'>
+                        <div className='p-2'>
+                            <img src={background} alt="test"></img>
+                            <h1 className='text-success'>Digital Clock</h1>
+                            <GetTime type={'digiClock'} />
+                            <h1 className='text-info'>Countdown Timer</h1>
+                            <GetTime type={'countdown'} />
+                            {/* <h1 className='text-warning'>Tic-Tac-Toe</h1>
+                        <TicTacToe /> */}
+                            {/* <h1 className='text-primary'>Mind Reader</h1>
+                        <MindReader /> */}
                         </div>
-                        <div className='col-0 col-sm-0 col-md-1 col-lg-2' />
-
                     </div>
+                    <div className='col-0 col-sm-0 col-md-1 col-lg-2' />
+
                 </div>
-            </React.Fragment>
-        );
-    }
+            </div>
+        </>
+    );
+
 }
 
 export default Layout;
--- src/MindReader.js
+++ src/MindReader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextBox from './TextBox';
 import Button from './Button';
+import SymbolArray from './SymbolArray';
 
 class MindReader extends React.Component {
     constructor(props) {
@@ -9,42 +10,17 @@ class MindReader extends React.Component {
         this.resetPage = this.resetPage.bind(this);
         this.state = {
             page: this.pageContent[0],
+            // outputSym: {'text': 'TEST'},
         }
     }
 
-    finalAns = 'DEFAULT';
+    // finalAns = 'DEFAULT';
     currentPage = 0;
+    // output = '';
 
-    genSym() {
-        let symArray = [];
-        let output = '';
-        // Symbol options
-        let symChar = ['=', '@', '*', '$', '%', '^', '&', '+', '#'];
-        // Generates the idx of the answer
-        let randNum = Math.floor(Math.random() * 9);
-        // Sets finalAns to that idx
-        this.finalAns = symChar[randNum];
-        // Then removes that symbol from the options
-        symChar.splice(randNum, 1);
-        // Add 100 items to the array
-        for (let i = 0; i < 100; i++) {
-            // Pick a random array symbol
-            let sym = symChar[Math.floor(Math.random() * 8)];
-            // If the sym is the same as the previous one, add a new symbol
-            // Did this b/c if you simply generate a new random number you run the risk of them happening to be the same
-            // And if you loop until you get a new number, you could (maybe - super unlikely) loop forever
-            if (sym === symArray[i - 1]) {sym = '!'};
-            symArray.push(sym);
-        }
-        // Replace every multiple of 9 with the answer
-        for (let i = 0; i < 12; i++) {
-            symArray.splice((i * 9), 1, this.finalAns);
-        }
-        // Format the output into num --> sym
-        symArray.map((item, idx) => output += `${idx} --> ${item} `);
-        console.log(this.finalAns);
-        return output;
-    }
+    // componentDidMount() {
+    //     this.genSym();
+    // }
 
     resetImg() {
         // return <img src='../public/undo-solid.svg' alt='reset'></img>;
@@ -52,12 +28,15 @@ class MindReader extends React.Component {
     }
 
     nextPage() {
+        this.currentPage === 0 && SymbolArray.genSym();
+        console.log(this.output);
         this.currentPage++;
         this.setState({ page: this.pageContent[this.currentPage] });
     }
 
     resetPage() {
-        this.genSym();
+        // this.genSym();
+        // console.log('Reset Pressed');
         this.currentPage = 0;
         this.setState({ page: this.pageContent[this.currentPage] });
     }
@@ -67,7 +46,7 @@ class MindReader extends React.Component {
         { 'text': 'Pick a number from 01-99', 'subtext': 'When you have your number, click next', 'btn': 'NEXT', 'reset': `${this.resetImg()}` },
         { 'text': 'Add both digits together to get a new number', 'subtext': `Ex: 25 is 2 + 5 = 7 || Ex: 05 is 0 + 5 = 5 || Click next to proceed`, 'btn': 'NEXT', 'reset': `${this.resetImg()}` },
         { 'text': 'Subtract your new number from your old number', 'subtext': 'Ex: 25 - 07 = 18 || Ex: 05 - 05 = 0 || Click next to proceed', 'btn': 'NEXT', 'reset': `${this.resetImg()}` },
-        { 'text': `${this.genSym()}`, 'subtext': '', 'btn': 'REVEAL', 'reset': `${this.resetImg()}` },
+        { 'text': '', 'subtext': '', 'btn': 'REVEAL', 'reset': `${this.resetImg()}` },
         { 'text': `${this.finalAns}`, 'subtext': `Your symbol is: ${this.finalAns}`, 'btn': '', 'reset': `${this.resetImg()}` },
     ]
 
