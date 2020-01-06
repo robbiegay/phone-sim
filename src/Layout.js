e80481df492ffae3e28e83c72805596eafea8f8a
Robbie Gay <robbiecgay@gmail.com>
2020-01-06 09:47:13 -0500

    Starting to get background image to work

--- src/Layout.js
+++ src/Layout.js
@@ -7,23 +7,31 @@ import background from './background-1.jpg';
 // Adds a Bootstrap layout to the App
 
 function Layout() {
+    var sectionStyle = {
+        // width: "100vw",
+        // height: "100vh",
+        backgroundImage: `url(${background})`,
+        backgroundSize: '100% auto',
+        borderRadius: '35px',
+    };
+
     return (
         <>
-        {/* <div id="bckImg" className="container-fluid mt-3" style="background-image: url(img/chidi-the-good-place.jpg); background-repeat: no-repeat; min-height: 700px"></div> */}
-            <div className='container-fluid'>
+            {/* <div id="bckImg" className="container-fluid mt-3" style="background-image: url(img/chidi-the-good-place.jpg); background-repeat: no-repeat; min-height: 700px"></div> */}
+            <div className='container-fluid bg-secondary'>
                 <div className='row'>
                     <div className='col-0 col-sm-0 col-md-1 col-lg-2' />
                     <div className='col-12 col-sm-12 col-md-10 col-lg-8 text-center'>
                         <div className='p-2'>
-                            <img src={background} alt="test"></img>
-                            <h1 className='text-success'>Digital Clock</h1>
-                            <GetTime type={'digiClock'} />
-                            <h1 className='text-info'>Countdown Timer</h1>
-                            <GetTime type={'countdown'} />
-                            {/* <h1 className='text-warning'>Tic-Tac-Toe</h1>
-                        <TicTacToe /> */}
-                            {/* <h1 className='text-primary'>Mind Reader</h1>
-                        <MindReader /> */}
+                            <section style={ sectionStyle }>
+                                <GetTime type={'digiClock'} />
+                                <h1 className='text-info'>Countdown Timer</h1>
+                                <GetTime type={'countdown'} />
+                                {/* <h1 className='text-warning'>Tic-Tac-Toe</h1>
+                                <TicTacToe /> */}
+                                {/* <h1 className='text-primary'>Mind Reader</h1>
+                                <MindReader /> */}
+                            </section>
                         </div>
                     </div>
                     <div className='col-0 col-sm-0 col-md-1 col-lg-2' />
