0db3b0f1a61ad3f9d32673a6ecbc7262ebdcd505
Robbie Gay <robbiecgay@gmail.com>
2020-01-22 17:14:22 -0500

    Updates

---
+++ precache-manifest.4166da5c7cac8d8087f6319c2bdf5a2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27de5f5232355e18855b4f10c0019431",
+    "url": "/phone-sim/index.html"
+  },
+  {
+    "revision": "4cc27bf5c2133147162f",
+    "url": "/phone-sim/static/css/2.903a27b1.chunk.css"
+  },
+  {
+    "revision": "d4f5081d06927f76410a",
+    "url": "/phone-sim/static/css/main.51f91f95.chunk.css"
+  },
+  {
+    "revision": "4cc27bf5c2133147162f",
+    "url": "/phone-sim/static/js/2.05c97587.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/phone-sim/static/js/2.05c97587.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4f5081d06927f76410a",
+    "url": "/phone-sim/static/js/main.87c18cb4.chunk.js"
+  },
+  {
+    "revision": "cd46710c0563b270fc42",
+    "url": "/phone-sim/static/js/runtime-main.fb6a124d.js"
+  },
+  {
+    "revision": "935f36cf74797ee3fc4789bf8dc5786f",
+    "url": "/phone-sim/static/media/background-1.935f36cf.jpg"
+  }
+]);
